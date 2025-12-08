c8c986f4d6ecf05126d8ed9c34713f99342247c8
Dave Pope <pope.david12@gmail.com>
2025-12-08 00:18:34 +0000

    Add simple unit test with mockall, implmented trait on Encounter so it could be mocked and asserted on

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -3,16 +3,14 @@ use arena_kafka::KafkaDependency;
 use arena_postgres::PostgresDependency;
 
 fn main() {
-    // Create concrete dependencies
     let mut postgres_db: Dependency = Box::new(PostgresDependency::new(
-        String::from("postgres_db")
+        String::from("parent")
     ));
 
     let kafka: Dependency = Box::new(KafkaDependency::new(
-        String::from("kafka")
+        String::from("child")
     ));
 
-    // Add kafka as a child of postgres
     postgres_db.add_child(kafka);
 
     let dependencies: Vec<Dependency> = vec![postgres_db];
@@ -25,6 +23,6 @@ fn main() {
     let encounter = Encounter::new("End to end happy path", dependencies, components);
     let encounters: Vec<Box<dyn EncounterTrait>> = vec![Box::new(encounter)];
 
-    let mut arena = Arena::new(String::from(""), encounters);
+    let mut arena = Arena::new(String::from("Example Arena"), encounters);
     arena.commence();
 }
