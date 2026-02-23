f3150e856979d47f8060a79e8a4abd05f6d9dc5c
daveepope <pope.david12@gmail.com>
2026-02-23 01:43:10 +0000

    Added simple docker component

--- arena-container-component/src/builder.rs
+++ arena-container-component/src/builder.rs
@@ -1,16 +1,39 @@
 use arena::Component;
+use arena::healthcheck::ReadinessCheck;
 use crate::container_component::ContainerComponent;
+use bollard::query_parameters::BuildImageOptionsBuilder;
+use bollard::{body_full, Docker};
+use futures::StreamExt;
+use std::path::PathBuf;
 
 pub struct ContainerComponentBuilder {
-    endpoint: String,
+    identifier: String,
     children: Option<Vec<Component>>,
+    dockerfile: String,
+    build_context: Option<PathBuf>,
+    image_tag: Option<String>,
+    network: Option<String>,
+    network_alias: Option<String>,
+    env_vars: Vec<(String, String)>,
+    runtime_args: Vec<(String, String)>,
+    port_mappings: Vec<(u16, u16)>,
+    readiness_checks: Vec<(Box<dyn ReadinessCheck>, String)>,
 }
 
 impl ContainerComponentBuilder {
-    pub(crate) fn new(endpoint: impl Into<String>) -> Self {
+    pub(crate) fn new(identifier: impl Into<String>, dockerfile: impl Into<String>) -> Self {
         Self {
-            endpoint: endpoint.into(),
+            identifier: identifier.into(),
             children: None,
+            dockerfile: dockerfile.into(),
+            build_context: None,
+            image_tag: None,
+            network: None,
+            network_alias: None,
+            env_vars: Vec::new(),
+            runtime_args: Vec::new(),
+            port_mappings: Vec::new(),
+            readiness_checks: Vec::new(),
         }
     }
 
@@ -19,10 +42,163 @@ impl ContainerComponentBuilder {
         self
     }
 
-    pub fn build(self) -> ContainerComponent {
+    pub fn with_build_context(mut self, path: impl Into<PathBuf>) -> Self {
+        self.build_context = Some(path.into());
+        self
+    }
+
+    pub fn with_image_tag(mut self, tag: impl Into<String>) -> Self {
+        self.image_tag = Some(tag.into());
+        self
+    }
+
+    pub fn with_network(mut self, network: impl Into<String>) -> Self {
+        self.network = Some(network.into());
+        self
+    }
+
+    pub fn with_network_alias(mut self, alias: impl Into<String>) -> Self {
+        self.network_alias = Some(alias.into());
+        self
+    }
+
+    pub fn with_env_var(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.env_vars.push((key.into(), value.into()));
+        self
+    }
+
+    pub fn with_runtime_arg(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.runtime_args.push((key.into(), value.into()));
+        self
+    }
+
+    pub fn with_port_mapping(mut self, host_port: u16, container_port: u16) -> Self {
+        self.port_mappings.push((host_port, container_port));
+        self
+    }
+
+    pub fn with_readiness_check<R>(mut self, check: R, target: impl Into<String>) -> Self
+    where
+        R: ReadinessCheck + 'static,
+    {
+        self.readiness_checks.push((Box::new(check), target.into()));
+        self
+    }
+
+    fn resolve_path(path: PathBuf) -> PathBuf {
+        if path.is_absolute() {
+            path
+        } else {
+            let current_dir = std::env::current_dir()
+                .expect("get current directory");
+
+            current_dir
+                .ancestors()
+                .find_map(|ancestor| {
+                    let candidate = ancestor.join(&path);
+                    if candidate.exists() {
+                        Some(candidate)
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or_else(|| current_dir.join(&path))
+        }
+    }
+
+    fn create_build_context_tar(identifier: &str, dockerfile: &str, build_context: &Option<PathBuf>) -> Vec<u8> {
+        let buf = Vec::new();
+        let mut tar = tar::Builder::new(buf);
+
+        let dockerfile_bytes = dockerfile.as_bytes();
+        let mut header = tar::Header::new_gnu();
+        header.set_size(dockerfile_bytes.len() as u64);
+        header.set_mode(0o644);
+        header.set_cksum();
+        tar.append_data(&mut header, ".arena.Dockerfile", dockerfile_bytes)
+            .expect("add Dockerfile to tar");
+
+        if let Some(ref context_path) = build_context {
+            tar.append_dir_all(".", context_path)
+                .unwrap_or_else(|e| panic!(
+                    "[Component-{}] failed to add build context {:?} to tar: {}",
+                    identifier, context_path, e
+                ));
+        }
+
+        tar.into_inner().expect("finalize tar archive")
+    }
+
+    async fn build_image(identifier: &str, dockerfile: &str, image_tag: &str, build_context: &Option<PathBuf>, docker: &Docker) {
+        log::info!(
+            "[Component-{}] building Docker image '{}'",
+            identifier, image_tag
+        );
+
+        let tar_body = Self::create_build_context_tar(identifier, dockerfile, build_context);
+
+        let options = BuildImageOptionsBuilder::default()
+            .dockerfile(".arena.Dockerfile")
+            .t(image_tag)
+            .rm(true)
+            .build();
+
+        let mut stream = docker.build_image(options, None, Some(body_full(tar_body.into())));
+
+        while let Some(result) = stream.next().await {
+            match result {
+                Ok(info) => {
+                    if let Some(ref stream_msg) = info.stream {
+                        let msg = stream_msg.trim_end();
+                        if !msg.is_empty() {
+                            log::info!("[Component-{}] {}", identifier, msg);
+                        }
+                    }
+                    if let Some(ref error) = info.error {
+                        panic!(
+                            "[Component-{}] docker build error: {}",
+                            identifier, error
+                        );
+                    }
+                }
+                Err(e) => {
+                    panic!(
+                        "[Component-{}] docker build failed: {}",
+                        identifier, e
+                    );
+                }
+            }
+        }
+
+        log::info!(
+            "[Component-{}] Docker image '{}' built successfully",
+            identifier, image_tag
+        );
+    }
+
+    pub async fn build(self) -> ContainerComponent {
+        let build_context = self.build_context.map(Self::resolve_path);
+
+        let image_tag = self.image_tag
+            .unwrap_or_else(|| format!("arena-{}", self.identifier));
+
+        let docker = Docker::connect_with_local_defaults()
+            .expect("connect to Docker daemon");
+
+        Self::build_image(&self.identifier, &self.dockerfile, &image_tag, &build_context, &docker).await;
+
         ContainerComponent {
-            endpoint: self.endpoint,
+            identifier: self.identifier,
             children: self.children,
+            image_tag,
+            network: self.network,
+            network_alias: self.network_alias,
+            env_vars: self.env_vars,
+            runtime_args: self.runtime_args,
+            port_mappings: self.port_mappings,
+            readiness_checks: self.readiness_checks,
+            docker,
+            container_id: None,
             stopped: false,
         }
     }
--- arena-container-component/src/container_component.rs
+++ arena-container-component/src/container_component.rs
@@ -1,24 +1,264 @@
 use async_trait::async_trait;
 use arena::component::RunnableComponent;
+use arena::healthcheck::ReadinessCheck;
 use crate::builder::ContainerComponentBuilder;
+use bollard::container::LogOutput;
+use bollard::models::{ContainerCreateBody, EndpointSettings, HostConfig, NetworkConnectRequest, PortBinding};
+use bollard::query_parameters::{
+    CreateContainerOptionsBuilder, LogsOptionsBuilder,
+    RemoveContainerOptionsBuilder, StopContainerOptionsBuilder,
+};
+use bollard::Docker;
+use futures::StreamExt;
+use std::collections::HashMap;
 
 pub struct ContainerComponent {
-    pub(crate) endpoint: String,
+    pub(crate) identifier: String,
     pub(crate) children: Option<Vec<Box<dyn RunnableComponent>>>,
+    pub(crate) image_tag: String,
+    pub(crate) network: Option<String>,
+    pub(crate) network_alias: Option<String>,
+    pub(crate) env_vars: Vec<(String, String)>,
+    pub(crate) runtime_args: Vec<(String, String)>,
+    pub(crate) port_mappings: Vec<(u16, u16)>,
+    pub(crate) readiness_checks: Vec<(Box<dyn ReadinessCheck>, String)>,
+    pub(crate) docker: Docker,
+    pub(crate) container_id: Option<String>,
     pub(crate) stopped: bool,
 }
 
 impl ContainerComponent {
-    pub fn new(endpoint: String) -> Self {
-        ContainerComponent {
-            endpoint,
-            children: None,
-            stopped: false,
+    pub fn builder(identifier: impl Into<String>, dockerfile: impl Into<String>) -> ContainerComponentBuilder {
+        ContainerComponentBuilder::new(identifier, dockerfile)
+    }
+
+    async fn create_and_start_container(&mut self) {
+        log::info!(
+            "[Component-{}] creating container from image '{}'",
+            self.identifier, self.image_tag
+        );
+
+        let env: Vec<String> = self.env_vars
+            .iter()
+            .map(|(k, v)| format!("{}={}", k, v))
+            .collect();
+
+        let cmd: Vec<String> = self.runtime_args
+            .iter()
+            .map(|(_k, v)| v.clone())
+            .collect();
+
+        let mut host_config = HostConfig {
+            ..Default::default()
+        };
+
+        let mut exposed_ports: HashMap<String, HashMap<(), ()>> = HashMap::new();
+        let mut port_bindings: HashMap<String, Option<Vec<PortBinding>>> = HashMap::new();
+
+        for (host_port, container_port) in &self.port_mappings {
+            let port_key = format!("{}/tcp", container_port);
+            exposed_ports.insert(port_key.clone(), HashMap::new());
+
+            let binding = PortBinding {
+                host_ip: Some("0.0.0.0".to_string()),
+                host_port: Some(host_port.to_string()),
+            };
+            port_bindings
+                .entry(port_key)
+                .or_insert_with(|| Some(Vec::new()))
+                .as_mut()
+                .unwrap()
+                .push(binding);
+        }
+
+        if !port_bindings.is_empty() {
+            host_config.port_bindings = Some(port_bindings);
+        }
+
+        let body = ContainerCreateBody {
+            image: Some(self.image_tag.clone()),
+            env: if env.is_empty() { None } else { Some(env) },
+            cmd: if cmd.is_empty() { None } else { Some(cmd) },
+            exposed_ports: if exposed_ports.is_empty() { None } else { Some(exposed_ports) },
+            host_config: Some(host_config),
+            ..Default::default()
+        };
+
+        let options = CreateContainerOptionsBuilder::default()
+            .name(&self.identifier)
+            .build();
+
+        let response = self.docker
+            .create_container(Some(options), body)
+            .await
+            .unwrap_or_else(|e| panic!(
+                "[Component-{}] failed to create container: {}",
+                self.identifier, e
+            ));
+
+        self.container_id = Some(response.id.clone());
+
+        log::info!(
+            "[Component-{}] container created (id: {})",
+            self.identifier, &response.id[..12.min(response.id.len())]
+        );
+
+        if let Some(ref network) = self.network {
+            let endpoint_config = EndpointSettings {
+                aliases: self.network_alias.as_ref().map(|a| vec![a.clone()]),
+                ..Default::default()
+            };
+
+            let connect_request = NetworkConnectRequest {
+                container: Some(response.id.clone()),
+                endpoint_config: Some(endpoint_config),
+                ..Default::default()
+            };
+
+            self.docker
+                .connect_network(network, connect_request)
+                .await
+                .unwrap_or_else(|e| panic!(
+                    "[Component-{}] failed to connect to network '{}': {}",
+                    self.identifier, network, e
+                ));
+
+            log::info!(
+                "[Component-{}] connected to network '{}'",
+                self.identifier, network
+            );
+        }
+
+        self.docker
+            .start_container(&response.id, None::<bollard::query_parameters::StartContainerOptions>)
+            .await
+            .unwrap_or_else(|e| panic!(
+                "[Component-{}] failed to start container: {}",
+                self.identifier, e
+            ));
+
+        log::info!(
+            "[Component-{}] container started (id: {})",
+            self.identifier, &response.id[..12.min(response.id.len())]
+        );
+    }
+
+    fn spawn_log_follower(&self) {
+        let container_id = match &self.container_id {
+            Some(id) => id.clone(),
+            None => return,
+        };
+        let identifier = self.identifier.clone();
+        let docker = self.docker.clone();
+
+        tokio::spawn(async move {
+            let options = LogsOptionsBuilder::default()
+                .follow(true)
+                .stdout(true)
+                .stderr(true)
+                .build();
+
+            let mut stream = docker.logs(&container_id, Some(options));
+
+            while let Some(result) = stream.next().await {
+                match result {
+                    Ok(output) => {
+                        let line = match output {
+                            LogOutput::StdOut { message } => {
+                                String::from_utf8_lossy(&message).trim_end().to_string()
+                            }
+                            LogOutput::StdErr { message } => {
+                                String::from_utf8_lossy(&message).trim_end().to_string()
+                            }
+                            _ => continue,
+                        };
+                        if !line.is_empty() {
+                            Self::log_line(&identifier, &line);
+                        }
+                    }
+                    Err(e) => {
+                        log::warn!(
+                            "[Component-{}] error reading container logs: {}",
+                            identifier, e
+                        );
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    fn log_line(identifier: &str, line: &str) {
+        if line.contains(" ERROR ") {
+            log::error!("[{}] {}", identifier, line);
+        } else if line.contains(" WARN ") {
+            log::warn!("[{}] {}", identifier, line);
+        } else if line.contains(" DEBUG ") {
+            log::debug!("[{}] {}", identifier, line);
+        } else if line.contains(" TRACE ") {
+            log::trace!("[{}] {}", identifier, line);
+        } else {
+            log::info!("[{}] {}", identifier, line);
         }
     }
 
-    pub fn builder(endpoint: impl Into<String>) -> ContainerComponentBuilder {
-        ContainerComponentBuilder::new(endpoint)
+    async fn wait_until_ready(&self) {
+        if self.readiness_checks.is_empty() {
+            return;
+        }
+
+        let timeout_ms = 10_000;
+        for (check, target) in &self.readiness_checks {
+            match check.is_ready(&self.identifier, target, timeout_ms).await {
+                Ok(()) => {
+                    log::debug!(
+                        "[Component-{}] readiness check passed for target: {}",
+                        self.identifier, target
+                    );
+                }
+                Err(msg) => {
+                    panic!(
+                        "[Component-{}] readiness check failed for target {}: {}",
+                        self.identifier, target, msg
+                    );
+                }
+            }
+        }
+        log::debug!("[Component-{}] all readiness checks passed.", self.identifier);
+    }
+
+    async fn stop_container(&self) {
+        let container_id = match &self.container_id {
+            Some(id) => id,
+            None => return,
+        };
+
+        log::info!(
+            "[Component-{}] stopping container (id: {})",
+            self.identifier, &container_id[..12.min(container_id.len())]
+        );
+
+        let stop_options = StopContainerOptionsBuilder::default()
+            .t(10)
+            .build();
+        if let Err(e) = self.docker.stop_container(container_id, Some(stop_options)).await {
+            log::warn!(
+                "[Component-{}] error stopping container: {}",
+                self.identifier, e
+            );
+        }
+
+        let remove_options = RemoveContainerOptionsBuilder::default()
+            .force(true)
+            .build();
+        if let Err(e) = self.docker.remove_container(container_id, Some(remove_options)).await {
+            log::warn!(
+                "[Component-{}] error removing container: {}",
+                self.identifier, e
+            );
+        }
+
+        log::info!("[Component-{}] container removed", self.identifier);
     }
 }
 
@@ -29,8 +269,13 @@ impl RunnableComponent for ContainerComponent {
             child.start().await;
         }
 
-        log::info!("[Component-{}] starting.", self.endpoint);
-        log::info!("[Component-{}] started.", self.endpoint);
+        log::info!("[Component-{}] starting.", self.identifier);
+
+        self.create_and_start_container().await;
+        self.spawn_log_follower();
+        self.wait_until_ready().await;
+
+        log::info!("[Component-{}] started.", self.identifier);
     }
 
     async fn stop(&mut self) {
@@ -38,8 +283,11 @@ impl RunnableComponent for ContainerComponent {
             return;
         }
 
-        log::info!("[Component-{}] stopping.", self.endpoint);
-        log::info!("[Component-{}] stopped.", self.endpoint);
+        log::info!("[Component-{}] stopping.", self.identifier);
+
+        self.stop_container().await;
+
+        log::info!("[Component-{}] stopped.", self.identifier);
 
         for child in self.children.iter_mut().flatten().rev() {
             child.stop().await;
